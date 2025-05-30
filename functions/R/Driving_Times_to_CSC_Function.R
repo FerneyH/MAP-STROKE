05470f738b2dd01105a92c4d1aad569993101ce3
Ferney Henao-Ceballos <ferney-henao-ceballos@uiowa.edu>
2025-05-30 16:42:46 -0500

    Update Driving_Times_to_CSC_Function.R

--- functions/R/Driving_Times_to_CSC_Function.R
+++ functions/R/Driving_Times_to_CSC_Function.R
@@ -9,7 +9,7 @@
 #' @import units
 #' @import dplyr
 
-options(openrouteservice.url = "http://ph-gb.iowa.uiowa.edu:8080/ors")
+options(openrouteservice.url = "http://XXXX/ors")
 
 
 GetDrivingTimeCSC<-function(closest_five_PSCs,facilities,warn = TRUE){
